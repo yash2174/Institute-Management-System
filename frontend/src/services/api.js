a89428ebbfb4311fb20971b8f768f66ef28cdb71
yash2174 <yashpaithane2004@gmail.com>
2026-02-21 22:05:01 +0530

    Updated api URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const api = axios.create({ baseURL: import.meta.env.VITE_API_URL || 'https://institute-management-system-3yxk.onrender.com' });
+const api = axios.create({ baseURL: import.meta.env.VITE_API_URL});
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
   if (token) config.headers.Authorization = `Bearer ${token}`;
