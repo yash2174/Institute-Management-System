ce1289a0d2a0cb24ed64a18055479dd8e6b0148f
yash2174 <yashpaithane2004@gmail.com>
2026-02-23 22:51:01 +0530

    updated vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 }
+  server: { port: 5174 },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: './index.html'
+    }
+  },
+  publicDir: 'public'
 })
